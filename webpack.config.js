65abfb6ed05fefb08567ed131656f56b33fd4e57
bpowell15 <bpaulpowell@gmail.com>
2018-01-18 15:21:09 -0500

    Way too many things for first commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+
+
+module.exports = {
+  entry: './lib/bubbleburst.js',
+  output: {
+    filename: "./lib/bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
