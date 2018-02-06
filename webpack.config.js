c39eab4faa20c850d36d130ec273cc4813824f6e
bpowell15 <bpaulpowell@gmail.com>
2018-02-06 16:57:22 -0500

    Fix padding and clicks

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
         exclude: /(node_modules)/,
         loader: 'babel-loader',
         query: {
-          presets: ['env', 'react']
+          presets: ['env']
         }
       }
     ]
